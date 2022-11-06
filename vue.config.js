420f66d24ac0923440d60a5eee2aa3b0b0146ddb
Diego Lima <diegoperes142@gmail.com>
2022-11-06 12:09:01 -0300

    Modificação no vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/planet_fun/" : "/",
   transpileDependencies: [
     'vuetify'
   ]
